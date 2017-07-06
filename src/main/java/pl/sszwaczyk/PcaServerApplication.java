955d7016dad1b0d03b2deb1a0f3488d98a898c58
Sebastian Szwaczyk <sszwaczyk@gmail.com>
2017-07-06 13:27:18 +0200

    Initial commit

---
+++ src/main/java/pl/sszwaczyk/PcaServerApplication.java
@@ -0,0 +1,12 @@
+package pl.sszwaczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcaServerApplication.class, args);
+	}
+}
